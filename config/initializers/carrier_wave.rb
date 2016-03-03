2c827c42da8a8c1b72a35c0757cafbae73cd2552
Stan <solve153@gmail.com>
2016-03-03 21:16:23 +0800

    fix Add S3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,13 +2,12 @@ CarrierWave.configure do |config|
   if Rails.env.production? 
     config.storage :fog                       
     config.fog_credentials = {
-      provider:              ENV[ 'provider' ],                        
-      aws_access_key_id:     ENV[ 'aws_access_key_id' ],      # 你的 key           
+      provider:              'AWS',                        
+      aws_access_key_id:     ENV['aws_access_key_id'],      # 你的 key           
 
-      aws_secret_access_key: ENV[ 'aws_secret_access_key' ] ,      # 你的 secret key         
+      aws_secret_access_key: ENV['aws_secret_access_key'] ,      # 你的 secret key         
 
-      region:                'ap-northeast-1' # 你的 S3 bucket 的 Region 位置   
-
+      region:                ENV['region']
     }
     config.fog_directory  = 'autherticate' # 你設定的 bucket name 
 
